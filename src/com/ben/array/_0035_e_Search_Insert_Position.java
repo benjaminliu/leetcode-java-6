f973c0cd475c356d86d0eccda5c9a127354c2150
ashley <benjamin.liu@live.cn>
2023-05-15 19:25:41 -0400

    _0035_e_Search_Insert_Position

---
+++ src/com/ben/array/_0035_e_Search_Insert_Position.java
@@ -0,0 +1,34 @@
+package com.ben.array;
+
+public class _0035_e_Search_Insert_Position {
+
+    class Solution {
+        public int searchInsert(int[] nums, int target) {
+            int lo = 0;
+            int hi = nums.length - 1;
+
+            if (nums[lo] > target) {
+                return 0;
+            }
+
+            if (nums[hi] < target) {
+                return nums.length;
+            }
+
+            while (lo <= hi) {
+                int mid = (lo + hi) / 2;
+                if (nums[mid] == target) {
+                    return mid;
+                }
+
+                if (nums[mid] > target) {
+                    hi = mid - 1;
+                } else {
+                    lo = mid + 1;
+                }
+            }
+
+            return lo;
+        }
+    }
+}
