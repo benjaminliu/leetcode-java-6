054c835db7c8124cba22530d82ea186365523e18
ashley <benjamin.liu@live.cn>
2023-05-16 20:38:46 -0400

    _0367_e_Valid_Perfect_Square

---
+++ src/com/ben/array/_0367_e_Valid_Perfect_Square.java
@@ -0,0 +1,29 @@
+package com.ben.array;
+
+public class _0367_e_Valid_Perfect_Square {
+    class Solution {
+        public boolean isPerfectSquare(int num) {
+            long target = num;
+            long lo = 0;
+            long hi = num;
+
+            while (lo <= hi) {
+                long mid = (lo + hi) / 2;
+
+                long sqr = mid * mid;
+
+                if (sqr == target) {
+                    return true;
+                }
+
+                if (sqr < target) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
