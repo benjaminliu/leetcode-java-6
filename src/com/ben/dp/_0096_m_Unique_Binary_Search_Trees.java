aab7aa452e7de5fab7b3aef3fd7eb49955c40c67
Ben <benjamin.liu@live.cn>
2024-04-02 20:23:13 -0400

    dp

---
+++ src/com/ben/dp/_0096_m_Unique_Binary_Search_Trees.java
@@ -0,0 +1,27 @@
+package com.ben.dp;
+
+public class _0096_m_Unique_Binary_Search_Trees {
+    public static void main(String[] args) {
+        int n = 3;
+
+        System.out.println(new Solution().numTrees(n));
+    }
+
+    static
+    class Solution {
+        public int numTrees(int n) {
+
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+            dp[1] = 1;
+
+            for (int i = 2; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    dp[i] += dp[j - 1] * dp[i - j];
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
